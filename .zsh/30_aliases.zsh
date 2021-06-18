1670a2d1f721d6c3aaffe97421e32a36463e9de4
hohihohi <a56.tazima@gmail.com>
2021-06-19 04:47:47 +0900

    Add alias to show tree with color

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -1,3 +1,4 @@
 ### CORE ###
 alias ..='cd ..'
-alias ll='exa -la' # Show hidden all files
+alias ll='exa -la'       # Show hidden all files
+alias treec='exa --tree' # Show files and directories as tree
