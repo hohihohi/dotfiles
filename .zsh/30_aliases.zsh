8d6d2cdd2801af8cb91b8eeb97cb577daba920e1
hohihohi <a56.tazima@gmail.com>
2021-06-19 09:33:24 +0900

    Add pokemon alias to display pokeom image

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -2,3 +2,6 @@
 alias ..='cd ..'
 alias ll='exa -la'       # Show hidden all files
 alias treec='exa --tree' # Show files and directories as tree
+
+### ART ###
+alias pokemon='(){cat ~/.art/pokemon/$1.txt}'
