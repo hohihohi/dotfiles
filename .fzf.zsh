8706d2df5ddb0cc622ee49c47f6584a90ea85d46
hohihohi <a56.tazima@gmail.com>
2021-06-24 14:33:43 +0900

    Use fzf with fd

--- .fzf.zsh
+++ .fzf.zsh
@@ -11,3 +11,10 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# With fd
+# ------------
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude .art'
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
