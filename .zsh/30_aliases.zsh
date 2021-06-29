8cc992fd607f7d54f1c911ad829799007d117e38
hohihohi <a56.tazima@gmail.com>
2021-06-29 19:14:33 +0900

    Fix some alias options

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -1,7 +1,7 @@
 ### CORE ###
 alias ..='cd ..'
-alias ll='exa -la'       # Show hidden all files
-alias treec='exa --tree' # Show files and directories as tree
+alias ll='exa -la --git' # Show hidden all files
+alias tre='exa --tree' # Show files and directories as tree
 
 ### ART ###
 alias pokemon='(){cat ~/.art/pokemon/$1.txt}'
