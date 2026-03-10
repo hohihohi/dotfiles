3f2eb7b784acabd4991a81f97b36fcc6d4e67b77
hohihohi <a56.tazima@gmail.com>
2026-03-10 19:06:58 +0900

    feat(zshrc): add preprocess for zshrc

---
+++ dot_zsh.d/00_preprocess.zsh
@@ -0,0 +1,42 @@
+### zsh management ###
+# To activate the zsh autosuggestions
+if type mise > /dev/null 2>&1; then
+    source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+# Set up your shell to use Starship
+if type starship > /dev/null 2>&1; then
+    eval "$(starship init zsh)"
+fi
+# To activate the zsh autosuggestions
+# For completions to work, the above line must be added after compinit is called.
+if type zoxide > /dev/null 2>&1; then
+    eval "$(zoxide init zsh)"
+fi
+
+### Language management ###
+# Automatic Activation: With mise activate,
+# mise hooks into your shell prompt and automatically updates your environment
+# when you change directories
+if type mise > /dev/null 2>&1; then
+    # If mise exists
+    # updates your environment variable and PATH every time
+    # your prompt is run to ensure you use the correct versions
+    eval "$(/opt/homebrew/bin/mise activate zsh)" # In your ~/.zshrc
+    # NodeJS
+    # -- Use node from mise as global
+    if ! which node >/dev/null 2>&1; then
+        mise use --global node@latest
+    fi
+    # Python
+    # install python from mise if node hasn't installed yet
+    if ! which python >/dev/null 2>&1; then
+        mise use --global python@latest
+    fi
+    # go
+    # install go from mise if node hasn't installed yet
+    if ! which go >/dev/null 2>&1; then
+        mise use --global go@latest
+    fi
+else
+    echo "NOT found mise command. please install mise"
+fi
