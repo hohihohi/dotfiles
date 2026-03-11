625b5a0a05dd36405023ba08536434ba8c24aca4
hohihohi <a56.tazima@gmail.com>
2026-03-11 15:12:12 +0900

    feat(zshrc): add zsh file to overwrite zstyle and key-bind

---
+++ dot_zsh.d/dot_80_overwrite.zsh
@@ -0,0 +1,4 @@
+# 80. Overwrite after loading the plugin because of replacing unintended bindkeys and zstyles.
+
+# Call key-bind function which is defined on ~/.zsh.d/.60_keybind.zsh
+apply_keybindings
