f84f27f3c71043eb67048efcfffe6bcb548c4c58
hohihohi <a56.tazima@gmail.com>
2026-03-15 11:46:40 +0900

    feat(zshrc): add yazi utility function which is suggested from official yazi

--- dot_zsh.d/dot_50_function.zsh
+++ dot_zsh.d/dot_50_function.zsh
@@ -5,6 +5,15 @@ zshaddhistory() {
     [[ "$?" == 0 ]]
 }
 
+# Provides the ability to change the current working directory when exiting Yazi.
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # For completions to work, the above line must be added after compinit is called.
 init_zoxide() {
   # For completions to work, the above line must be added after compinit is called.
