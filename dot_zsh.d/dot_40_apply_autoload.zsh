e36614b69b988aa0ab38bda9463cfb08cf50af7c
hohihohi <a56.tazima@gmail.com>
2026-03-17 18:24:19 +0900

    fix(zshrc): fix zcompdump file path because of integrating file path

--- dot_zsh.d/dot_40_apply_autoload.zsh
+++ dot_zsh.d/dot_40_apply_autoload.zsh
@@ -3,4 +3,6 @@
 # Enable to use colors on terminal
 colors
 # Enable to use completion
-compinit
+# Specify the zcompdump directory name explicitly. This is because the cache-path is specified explicitly in zstyle.
+# The aim is to standardise the storage location for zcompdump.
+compinit -d ~/.cache/zsh/zcompdump
